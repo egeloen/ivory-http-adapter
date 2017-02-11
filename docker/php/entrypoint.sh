6742ab319920343148591adb5581ac4e2763d0bc
Eric GELOEN <geloen.eric@gmail.com>
2017-02-11 11:07:21 +0100

    [Travis] Add docker build

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -7,8 +7,8 @@ USER_ID=${USER_ID-1000}
 XDEBUG=${XDEBUG-0}
 
 # Disable XDebug
-if [ ! ${XDEBUG} = 1 ]; then
-    rm -f /usr/local/etc/php/conf.d/xdebug.ini
+if [ ! "$XDEBUG" = 1 ]; then
+    rm -f /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini /usr/local/etc/php/conf.d/xdebug.ini
 fi
 
 # Permissions
@@ -16,7 +16,7 @@ groupmod -g ${GROUP_ID} www-data
 usermod -u ${USER_ID} www-data
 
 # Start bash or forward command
-if [ $1 = "bash" ]; then
+if [ "$1" = "bash" ]; then
     su www-data
 else
     su www-data -c "$*"
